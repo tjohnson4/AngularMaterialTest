15be084a1d62f26c2111422bcd2965db2bf3eb3c
Todd Johnson <tjohnson4@gmail.com>
2015-04-07 23:29:50 -0700

    updated paths and added the api router

--- app.js
+++ app.js
@@ -7,11 +7,13 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var api = require("./routes/api");
 
 var app = express();
+app.set('port', process.env.PORT || 3000);
 
 // view engine setup
-app.set('views', path.join(__dirname, '../views'));
+app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
@@ -20,13 +22,12 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '/public')));
 app.use("/bower_components", express.static(__dirname +'/bower_components'));
-
-console.log(__dirname)
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/api', api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
